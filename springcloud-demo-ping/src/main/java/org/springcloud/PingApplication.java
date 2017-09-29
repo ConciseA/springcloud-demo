6a6c840e1eb5a9fb65cd75b098368a69f8cc2207
Menng <concisesky@gmail.com>
2017-09-29 11:40:03 +0800

    add springboot-demo to eureka

--- springcloud-demo-ping/src/main/java/org/springcloud/PingApplication.java
+++ springcloud-demo-ping/src/main/java/org/springcloud/PingApplication.java
@@ -2,8 +2,10 @@ package org.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PingApplication {
 
     public static void main(String[] args) {
