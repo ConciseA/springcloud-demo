d205c7d05f547f40cf67df4b7616dfa98283f721
Menng <conciseksy@gmail.com>
2018-04-18 23:47:17 +0800

    'update'

--- springcloud-pong/src/main/java/org/springcloud/controller/PongController.java
+++ springcloud-pong/src/main/java/org/springcloud/controller/PongController.java
@@ -1,5 +1,7 @@
 package org.springcloud.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springcloud.pojo.City;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +9,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class PongController {
+    private static Logger logger = LoggerFactory.getLogger(PongController.class);
 
     @Value("${hiAaron}")
     private String message;
 
     @GetMapping("/message")
     public String getMessage() {
+        logger.info("currentTime={}", new Date());
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return this.message + " - pong";
     }
 
